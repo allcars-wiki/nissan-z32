bb92f25e1af25f411590008c6c7299d3b62f7e50
Xavier Martin <nolazybits@gmail.com>
2020-03-09 10:57:54 +0000

    playing with actions

--- .docker/stop.sh
+++ .docker/stop.sh
@@ -13,4 +13,9 @@ elif [ ${os} = "Darwin" ]; then
 fi
 ROOT_DIR=$(realpath "${SCRIPT_DIR}/..")
 
+# check we have the required .env file otherwise create them from the template
+if [[ ! -f "${SCRIPT_DIR}/.env" ]]; then
+    cp "${SCRIPT_DIR}/.env.template" "${SCRIPT_DIR}/.env"
+fi
+
 cd "${SCRIPT_DIR}" && HOST_PATH="${ROOT_DIR}/" docker-compose -f "${SCRIPT_DIR}/docker-compose.yml" down
